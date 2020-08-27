17f30309af227f65c4b8816188411a99ac9db2a0
Edwy Lugo <edwylugo@gmail.com>
2020-08-27 09:14:31 -0300

    - Configuring the Project

---
+++ WhatsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsApp
+//
+//  Created by Edwy Lugo on 16/07/20.
+//  Copyright © 2020 Edwy Lugo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
